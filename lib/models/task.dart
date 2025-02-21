3c7f43a184f2dac1e24873a5447464de745cbbd7
Saku <sakuTech.info@gmail.com>
2025-02-21 14:17:37 +0900

    タスクをドラッグアンドドロップで配置できる機能追加

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -4,10 +4,12 @@ class Task {
   final String id;
   final String title;
   final Color color;
+  final Duration targetDuration;
 
   Task({
     required this.id,
     required this.title,
     required this.color,
+    required this.targetDuration,
   });
 }
